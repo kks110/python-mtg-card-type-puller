a23c52028aa69f1bc74ec5864fdf61bfe18a02e8
Kelvin <44570273+kks110@users.noreply.github.com>
2019-01-25 17:51:48 +0000

    Update mtg_cardTypePuller.py
    Added a loop so that over if there are over 175 cards, it will actually download them.
    Also moved the writer to it's own function.

--- mtg_cardTypePuller.py
+++ mtg_cardTypePuller.py
@@ -2,40 +2,49 @@
 import json
 #Used to get the URL
 import requests
-#Used to modify a file
-import fileinput
-#Used to download the images
-import urllib.request
-#Used to interact with the OS file system
-import os
 #Used to export the data to a CSV
 import csv
 
 
-def pullList():
-    creatureInput = input("What type of creatures are you looking for? - ")
-    #API call to get the JSON file
-    API = "https://api.scryfall.com/cards/search?q=t:" + creatureInput
-    response = requests.get(API)
-    JSON = response.json()
-    #Creates the CSV file
-    with open('mtg_' + creatureInput + '.csv', 'w', newline='', encoding='utf8') as csv_file:
+#This writes the file.
+def write_file(creature_input, card_data, set_count, first_row = False):
+    with open('mtg_' + creature_input + '.csv', 'a', newline='', encoding='utf8') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        #Counts the amount of cards in the set
-        cardCountStage = JSON['data']
-        setCount = len(cardCountStage)
-        for Counter in range(setCount):
+        if first_row:
+            writer.writerow(["Card Name", "Card colour", "Rarity", "Set Name", "Set Date"])
+        for x in range(set_count):
             #Prints the data out on screen and also puts it in to the CSV file
-            CardName = JSON['data'][Counter]['name']
-            cardColour = JSON['data'][Counter]['color_identity']
-            rarity = JSON['data'][Counter]['rarity']
-            setName = JSON['data'][Counter]['set_name']
-            setCode = JSON['data'][Counter]['set']
-            setAPI = "https://api.scryfall.com/sets/" + setCode
-            setResponse = requests.get(setAPI)
-            setJSON = setResponse.json()
-            setDate = setJSON['released_at']
-            writer.writerow([CardName, cardColour, rarity, setName, setDate])
+            card_name = card_data['data'][x]['name']
+            card_colour = card_data['data'][x]['color_identity']
+            rarity = card_data['data'][x]['rarity']
+            set_name = card_data['data'][x]['set_name']
+            set_code = card_data['data'][x]['set']
+            set_api = "https://api.scryfall.com/sets/" + set_code
+            set_response = requests.get(set_api)
+            set_json = set_response.json()
+            set_date = set_json['released_at']
+            writer.writerow([card_name, card_colour, rarity, set_name, set_date])
+
+def pullList():
+    card_counter = 0
+    card_input = input("What type of card are you looking for? - ")
+    #API call to get the JSON file
+    api = "https://api.scryfall.com/cards/search?q=t:" + card_input
+    response = requests.get(api)
+    card_data = response.json()
+    total_cards = card_data['total_cards']
+    #Counts the amount of cards in the set
+    set_count = len(card_data['data'])
+    write_file(card_input, card_data, set_count, True)
+    card_counter += set_count
+    #If there are more than 175 cards, code to get the second half. The JSONs only contain info for the first 175 cards
+    while card_counter < total_cards:
+        continued = card_data['next_page']
+        response_cont = requests.get(continued)
+        card_data = response_cont.json()
+        set_count = (len(card_data['data']))
+        write_file(card_input, card_data, set_count)
+        card_counter += set_count
 
 def main():
     pullList()
